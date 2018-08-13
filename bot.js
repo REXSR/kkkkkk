57035d4d19c43c862e8ace73ad108236293f9fe8
kerohany <41383774+kerohany@users.noreply.github.com>
2018-08-13 22:11:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1192,8 +1192,8 @@ if(cmd === `${prefix}coins`) {
 const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
 const coolDown = new Set();
 
-client.on('message', message => {
-    
+client.on('message',async message => {
+
 if(message.author.bot) return;
 if(!credits[message.author.id]) credits[message.author.id] = {
     credits: 50
@@ -1208,17 +1208,17 @@ fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
   credits[message.author.id] = {
       credits: m + 0.5,
   }
-  
-    if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
-message.channel.send(`**${message.author.username}, your :credit_card: balance is \`\`${userData.credits}\`\`.**`);
+
+    if(message.content.startsWith(prefix + "$$credit" || prefix + "credits")) {
+message.channel.send(**${message.author.username}, your 💳 balance is \`${userData.credits}``.);
 }
 });
 
-client.on('message', message => {
+client.on('message', async message => {
     let amount = 250;
-    if(message.content.startsWith(prefix + "daily")) {
+    if(message.content.startsWith(prefix + "$$daily")) {
     if(message.author.bot) return;
-    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
+    if(coolDown.has(message.author.id)) return message.channel.send(:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in ``1 Day``.);
     
     let userData = credits[message.author.id];
     let m = userData.credits + amount;
@@ -1230,10 +1230,10 @@ client.on('message', message => {
     if (err) console.error(err);
     });
     
-    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
+    message.channel.send(:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
         coolDown.add(message.author.id);
     });
-    
+
     setTimeout(() => {
        coolDown.remove(message.author.id);
     },86400000);
