09bbb8984498dce389019d236f5482c407a8c9f0
kerohany <41383774+kerohany@users.noreply.github.com>
2018-08-24 12:27:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1171,7 +1171,7 @@ client.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = "البريفكس";
+  let prefix = "$$";
   let messageArray = message.content.split (" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
