48279443039d4a5e78b9441aa045ab09329f0e6a
kerohany <41383774+kerohany@users.noreply.github.com>
2018-09-11 15:53:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,23 +55,11 @@ client.on('message', message => {
              اومر عادية
 ╚[❖════════════❖]╝
 
-❖ $$daily ➾ لي معرفة كم معاك كريديت
-
-❖ $$credit ➾ لي اخد كريديت
-
-❖ $$credits ➾ لي تحويل كريديت
-
-❖ $$bans ➾ لي معرف مين بالع بان
-
 ❖ $$sg ➾ لي البحث في جوجل
-
-❖ $$topinvites  ➾ لي عرض من اكثر في الدعوات
 
 ❖ $$skin ➾ لي عرض سكن ماين كرافت 
 
 ❖ $$count  ➾ لي معرفة عدد الأعضاء في السيرفر
-
-❖ $$embed  ➾ يحط لك الكلام في مربع
 
 ❖ $$allbots  ➾ لي اظهار جميع البوتات
 
@@ -81,11 +69,7 @@ client.on('message', message => {
 
 ❖ $$id ➾ لي عرض معلومات عن حسابك
 
-❖ $$dt ➾ لي معرفة الوقت
-
 ❖ $$avatar ➾ لي عرض صورتك او اي شخص تمنشن
-
-❖ $$emoji ➾ليكي البوت يرسل نفس الاموجي نفس الاموجي الي ارسلته
 
 ❖ $$tag  ➾ لي الزغرفة
 
@@ -133,15 +117,6 @@ client.on('message', message => {
 
 ❖ $$لعبة لوخيروك ➾ لوخيروك
 
-==================================================================
-
-Server support: https://discord.gg/dYmDdYj
-
-==================================================================
-
-bot invite link: https://discordapp.com/api/oauth2/authorize?client_id=481406227054592000&permissions=2146958839&scope=bot
-
-==================================================================
 **
 `);
 
@@ -172,16 +147,7 @@ console.log('[Gif Search] Developed By Ghost')
 });
 
 
-client.on('message', msg => {
-if(msg.content === 'هلا')
-msg.reply('هلا حبي')
-});
 
-
-client.on('message', msg => {
-if(msg.content === 'السلام عليكم')
-msg.reply('وعليكم السلام ورحمة الله وبركاته')
-});
     
 client.on('message', message => {
   if(!message.channel.guild) return;
@@ -592,38 +558,7 @@ __امامك  5 توان للاختيار__`)
 
 
 
-client.on('message', message => {
-      if(message.content.startsWith ("$$marry")) {
-      if(!message.channel.guild) return message.reply('** This command only for servers **')
-      var proposed = message.mentions.members.first()
-     
-      if(!message.mentions.members.first()) return message.reply('لازم تطلب ايد وحدة').catch(console.error);
-      if(message.mentions.users.size > 1) return message.reply('ولد ما يصحلك الا حرمة وحدة كل مرة').catch(console.error);
-       if(proposed === message.author) return message.reply(`**خنثى ؟ **`);
-        if(proposed === client.user) return message.reply(`** تبي تتزوجني؟ **`);
-              message.channel.send(`**${proposed} 
- بدك تقبلي عرض الزواج من ${message.author}
- العرض لمدة 15 ثانية 
- اكتب موافقة او لا** `)
 
-const filter = m => m.content.startsWith("موافقة");
-message.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
-.then(collected =>{ 
-    message.channel.send(`**${message.author} و ${proposed} الف الف مبروك الله يرزقكم الذرية الصالحة**`);
-})
-   .catch(collected => message.channel.send(`**السكوت علامة الرضا نقول مبروك ؟**`))
-   
-   const filte = m => m.content.startsWith("لا");
-message.channel.awaitMessages(filte, { max: 1, time: 15000, errors: ['time'] })
-.then(collected =>{ 
-   message.channel.send(`**${message.author} تم رفض عرضك**`);
-})
-        
-  
-             
-    
-  }
-});
 
 
 
@@ -691,11 +626,7 @@ if (command == "embed") {
 });
 
 
-client.on('guildCreate', guild => {
-  client.channels.get("461429580650119168").send(`**Woops new server ✅
-Server name: __${guild.name}__
-Server owner: __${guild.owner}__**`)
-});
+
 
 
 
@@ -801,33 +732,7 @@ client.on('message', message => {
 
 
 
-client.on('message',message =>{
-    var prefix = "$$";
-    if(message.content.startsWith(prefix + 'topinvites')) {
-  message.guild.fetchInvites().then(i =>{
-  var invites = [];
-   
-  i.forEach(inv =>{
-    var [invs,i]=[{},null];
-     
-    if(inv.maxUses){
-        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
-    }else{
-        invs[inv.code] =+ inv.uses;
-    }
-        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
-   
-  });
-  var embed = new Discord.RichEmbed()
-  .setColor("#000000")
-  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
-  .setThumbnail("https://cdn.discordapp.com/avatars/472224983943217175/4fcb0459f7aab6af86d56d97cf7de54b.png?size=2048")
-           message.channel.send({ embed: embed });
-   
-  });
-   
-    }
-  });
+
 
 
   const fs = require("fs");
@@ -1014,113 +919,6 @@ client.on('message', message => {//mute
 
 
 
-	const moment = require('moment');
-let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
-client.on("message", message => {
-  if (message.author.bot) return;
- if(!message.channel.guild)return;
-  if (!profile[message.author.id]) profile[message.author.id] = {
-    tite: 'HypeLC User',
-    rep: 0,
-   reps: 'NOT YET',
-   lastDaily:'Not Collected',
-    level: 0,
-    points: 0,
-    credits: 1,
-  };
-fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
-if (err) console.error(err);
-})
-});
-client.on("message", (message) => {
-  let men = message.mentions.users.first()
-  if (message.author.bot) return;
-    if (message.author.id === client.user.id) return;
-    if(!message.channel.guild) return;
-    var prefix ="$$"
-    if(message.content.startsWith(prefix + "credit")) {
-  if(men) {
-  if (!profile[men.id]) profile[men.id] = {
-   lastDaily:'Not Collected',
-   credits: 1,
- };
-  }
-  if(men) {
-message.channel.send(`** ${men.username}, your :credit_card: balance` + " is `" + `${profile[men.id].credits}$` + "`.**")
-} else {
- message.channel.send(`** ${message.author.username}, your :credit_card: balance` + " is `" + `${profile[message.author.id].credits}$` + "`.**")
-}
-}
-var prefix ="$$"
-if(message.content.startsWith(prefix + "daily")) {
-
-
-  if(profile[message.author.id].lastDaily != moment().format('day')) {
-   profile[message.author.id].lastDaily = moment().format('day')
-   profile[message.author.id].credits += 310
-    message.channel.send(`** :atm:  | ${message.author.username} you received your :yen: \`310\` daily credits!**`)
-} else {
-    message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${hours} , ${minutes} and ${Seconds}**`)
-}
-}
-let cont = message.content.slice(prefix.length).split(" ");
-let args = cont.slice(2);
-let sender = message.author
-if(message.content.startsWith(prefix + 'credits')) {
-          if (!args[0]) {
-            message.channel.send(`**Usage: ${prefix}credits @someone amount**`);
-         return;
-           }
-        // We should also make sure that args[0] is a number
-        if (isNaN(args[0])) {
-            message.channel.send(`**Usage: ${prefix}credits @someone amount**`);
-            return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
-             }
-             if(profile[message.author.id].credits < args[0]) return message.channel.send("**Your Credits is not enough  that**")
-if(args[0].startsWith("-")) return  message.channel.send('**!! I Cant Do it**');
-				 let defineduser = '';
-            let firstMentioned = message.mentions.users.first();
-            defineduser = (firstMentioned)
-            if (!defineduser) return message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
-            if(defineduser.id === message.author.id) return message.channel.send("***Transfering to your self hah ?!***")
-            var mentionned = message.mentions.users.first();
-if (!profile[sender.id]) profile[sender.id] = {}
-if (!profile[sender.id].credits) profile[sender.id].credits = 310;
-fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
-if (err) console.error(err);
-})
-var x = ['5587' ,' 9978' , '3785' , '7734' , '9864' , '7681' , '3758' , '7834' , '3489' , '1382' , '7389' , '8762' , '0889' , '0388' , '3316' , '0976' , '8603' , '1842' , '4565' , '9524' , '9524' , '0964' , '5930' , '5678' , '9567' , '6099' , '7058' , '0001' , '1324' , '9834' , '7668' , '0378' , '7055' , '9733' , '9876' , '9846' , '9685' , '8574' , '8975' , '9845' , '9862' , '0069' , '0807' , '0673' , '0813' , '1235' , '6879'];
-var x2 = ['5587' ,' 9978' , '3785' , '7734' , '9864' , '7681' , '3758' , '7834' , '3489' , '1382' , '7389' , '8762' , '0889' , '0388' , '3316' , '0976' , '8603' , '1842' , '4565' , '9524' , '9524' , '0964' , '5930' , '5678' , '9567' , '6099' , '7058' , '0001' , '1324' , '9834' , '7668' , '0378' , '7055' , '9733' , '9876' , '9846' , '9685' , '8574' , '8975' , '9845' , '9862' , '0069' , '0807' , '0673' , '0813' , '1235' , '6879'];
-        var x3 = Math.floor(Math.random()*x.length)
-        message.channel.send(` \`${args}\`** : الملبغ**  \n \`${x[x3]}\` ** : اكتب الرقم التالي حتي تتم عملية التحويل **`).then(msg1=> { 
-        var r = message.channel.awaitMessages(msg => msg.content == x2[x3], { maxMatches : 1, time : 60000, errors : ['time'] })
-        r.catch(() => {
-            message.delete()
-            r.delete()
-            msg.delete()
-            message.channel.sendEmbed(embed)
-        })
-        r.then(s=> {
-      var mando = message.mentions.users.id;
-      if  (!profile[defineduser.id]) profile[defineduser.id] = {}
-      if (!profile[defineduser.id].credits) profile[defineduser.id].credits = 200;
-      profile[defineduser.id].credits += (+args[0]);
-      profile[sender.id].credits += (-args[0]);
-      let mariam = message.author.username
-message.channel.send(`**:moneybag: | ${message.author.username}, has transferrerd ` + "`" + args[0] + "$` to " + `<@${defineduser.id}>**`)
-mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${args[0]} from user ${message.author.username} ; (ID (${message.author.id})\`\`\``);
-               message.channel.sendEmbed(embed)
-        })
-        })
-        
-		
-
-
-
-
-}
-
-      });
 
 
 
