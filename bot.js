06de0ea95e6e77a97e63d8ee3be998ad476ffaa0
kerohany <41383774+kerohany@users.noreply.github.com>
2018-09-13 19:41:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setActivity("$$help|$$invite",{type: 'WATCHING'})
+ client.user.setActivity("$$help|$$inv",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
