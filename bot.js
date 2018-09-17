57560e012ac5c4cce132c0021c6c3c329e918216
kerohany <41383774+kerohany@users.noreply.github.com>
2018-09-17 13:34:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -932,13 +932,6 @@ const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
-const client = new Discord.Client();
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`in ${client.guilds.size} servers `)
-    console.log(`[Codes] ${client.users.size}`)
-    client.user.setStatus("idle")
-});
 
 const prefix = "$$"
 client.on('message', async msg => { // eslint-disable-line
