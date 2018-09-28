5f72bb27a8bf182d76ea68dfda46fb4a06c1556b
kerohany <41383774+kerohany@users.noreply.github.com>
2018-09-28 07:41:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1165,7 +1165,12 @@ msg.channel.send(`${item.type}`).then(() => {
 	})
 }
 });
+
+
+
+
 client.on('message', message => {
+	var prefix = "$$"
 if (message.content.startsWith(prefix + 'نقاطي')) {
 	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
 	let userData = points[message.author.id];
@@ -1179,16 +1184,6 @@ if (message.content.startsWith(prefix + 'نقاطي')) {
     if (err) console.error(err)
   })
 });
-
-
-
-
-
-
-
-
-
-
 
 
 
